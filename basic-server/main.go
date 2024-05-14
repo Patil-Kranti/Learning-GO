b6f842a1878036dca1ba5770f04316c71465f901
Patil-Kranti <patilkranti03@gmail.com>
2024-05-14 21:55:58 +0530

    completed basic server

--- basic-server/main.go
+++ basic-server/main.go
@@ -31,5 +31,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/login" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successfull\n")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Fprintf(w, "username = %s\n", username)
+	fmt.Fprintf(w, "password = %s\n", password)
 
 }
