d3f9466dc5016b328e690ec35a6c256eabb150f6
Patil-Kranti <patilkranti03@gmail.com>
2024-05-29 00:36:49 +0530

    completed hot reload setup using air and templ proxy

---
+++ gotth_tutorial/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"gotth_tutorial/handlers"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	router := chi.NewMux()
+	router.Handle("/*", public())
+	router.Get("/foo", handlers.Make(handlers.Handlefoo))
+	fmt.Println("Hello, World!1")
+
+	listenAdder := os.Getenv("LISTEN_ADDR")
+	slog.Info("Http server started", "listenAddr", listenAdder)
+	http.ListenAndServe(listenAdder, router)
+}
