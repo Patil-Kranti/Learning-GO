e2462a5687f18b3f6ccc17d4983dbff6f74f0061
Patil-Kranti <patilkranti03@gmail.com>
2024-06-01 17:15:58 +0530

    completed the server setup

--- go-docker-tutorial/api.go
+++ go-docker-tutorial/api.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type ApiError struct {
+	Error string
+}
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 func makeHttpHandleFunc(f apiFunc) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
+			WriteJson(w, http.StatusOK, ApiError{Error: err.Error()})
 
 		}
 	}
@@ -28,9 +40,12 @@ func NewApiServer(listenAddr string) *ApiServer {
 	}
 }
 
-func (*ApiServer) Run() {
+func (s *ApiServer) Run() {
 	router := mux.NewRouter()
-	router.HandleFunc("/account", s.handleAccount)
+	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
+
+	log.Println("Server running on port: ", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
 }
 func (*ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
